5746aef5001f176b460e02b311dc47185e74f014
javabuk <joprieto@gmail.com>
2018-09-22 18:10:55 +0200

    Prueba con la impresión por consola de los parámetros del método.

--- src/main/java/org/pruebas/aop/Principal.java
+++ src/main/java/org/pruebas/aop/Principal.java
@@ -11,13 +11,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(Configuracion.class);
-		
-		/*IServicio servicio = appContext.getBean("servicio", IServicio.class);
-		servicio.ejecutarOperacion();
-		
-		IServicio servicioDiferente = appContext.getBean("servicioDiferente", IServicio.class);
-		servicioDiferente.ejecutarOperacion();*/
-		
+
+		System.out.println("Pruebas de AspectJ");
+
 		IServicio servicio = appContext.getBean("servicioPrueba", IServicio.class);
 		servicio.ejecutarOperacion("uno", "dos");
 		servicio.ejecutarOperacion("TEXTO PARAMETRO 1", "TEXTO PARAMETRO2");
